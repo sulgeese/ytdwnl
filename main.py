ade8e858f1650d1618ecf9d97c67a1e49a1f958f
sulgeese <sul.geese@gmail.com>
2025-03-22 02:13:47 +0300

    first commit

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from yt_dlp import YoutubeDL
 if __name__ == "__main__":
     ydl = YoutubeDL()
     ydl.add_default_info_extractors()
-    info = ydl.extract_info("https://www.youtube.com/watch?v=5ognujPxaec&ab_channel=EnsembleG.A.P.TOKYO")
+    info = ydl.extract_info("https://www.youtube.com/watch?v=5ognujPxaec&ab_channel=EnsembleG.A.P.TOKYO", download=False)
+    print(info)
